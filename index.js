c5a535db0676c860214e8fedf95836d14b150612
girl-on-a-string <frenchfries55@protonmail.com>
2024-09-05 13:55:56 -0500

    created basic layout for main and document

---
+++ index.js
@@ -0,0 +1,14 @@
+//section 1: variables
+
+let tabTitle = document.getElementsByTagName("title");
+let settings = document.getElementsByTagName("aside");
+let main = document.getElementById("main");
+let doc = document.getElementById("document");
+
+let key = null;
+
+//section 2: keyboard handling
+
+doc.addEventListener("click", () => { //so typing only happens in the document window/div
+
+});
