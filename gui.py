8be117edd970fd54ef07ccc24445bbae7faa1252
girl-on-a-string <frenchfries55@protonmail.com>
2024-09-16 17:47:22 -0500

    titles main window and set default size

--- gui.py
+++ gui.py
@@ -1,5 +1,19 @@
+#imports
+
 import tkinter as tk
 
+# create root window
+
 root = tk.Tk();
+
+root.geometry("2000x1000");
+
+root.title("(AmbientWriter)"); #include name of doc or "untitled document" in var before. ex. "title of book (ambientwriter)" later
+
+#create menu
+
+
+
+#instanciate root window
 
 root.mainloop();
