ccaa843cc0cded4b28c6636a86ac1e91bb8667f5
girl-on-a-string <frenchfries55@protonmail.com>
2024-09-18 13:39:14 -0500

    created and imported (into gui.py) pallettes.py

--- gui.py
+++ gui.py
@@ -1,15 +1,53 @@
 #imports
 
+import palettes
 import tkinter as tk
 
-class myGUI:
+class startGUI:
     def __init__(self):
-        self.root = tk.Tk();
-        self.root.geometry("2000x1000");
-        self.root.title("(AmbientWriter)"); #include name of doc or "untitled document" in var before. ex. "title of book (ambientwriter)" later
+        #create window
 
-        self.label = tk.Label(self.root, text="Your message", font=("arial", 18))
+        self.root = tk.Tk()
+        self.root.geometry("2000x1000")
+        self.root.title("AmbientWriter")
 
-        self.root.mainloop();
+        #create frame
 
-myGUI();
+        frame = tk.Frame(self.root);
+
+        #define grid
+
+        self.root.columnconfigure(0, weight=1)
+        self.root.columnconfigure(1, weight=1)
+        self.root.columnconfigure(2, weight=1)
+        self.root.columnconfigure(3, weight=1)
+        self.root.columnconfigure(4, weight=1)
+        self.root.columnconfigure(5, weight=1)
+        self.root.columnconfigure(6, weight=1)
+        self.root.columnconfigure(7, weight=1)
+
+        #place widgets
+
+
+
+        #create window
+
+        self.root.mainloop()
+
+# class docGUI:
+#     def __init__(self):
+#         self.root = tk.Tk()
+#         self.root.geometry("2000x1000")
+#         self.root.title("(AmbientWriter)"); #include name of doc or "untitled document" in var before. ex. "title of book (ambientwriter)" later
+
+#         self.textbox = tk.Text(self.root, height=5, font=("Arial", 16))
+#         self.textbox.pack(padx=10, pady=10)
+
+#         #make menu
+
+#         menu = tk.Menu(self.root);
+
+#         self.root.mainloop()
+
+startGUI()
+
