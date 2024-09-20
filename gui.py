c626b30e9b5f1d4c0d432c7ffddacf7d41de0dc1
girl-on-a-string <frenchfries55@protonmail.com>
2024-09-19 20:12:31 -0500

    added rest of text decoration buttons. converted heading options to radio buttons. edited other menus commands

--- gui.py
+++ gui.py
@@ -43,7 +43,7 @@ class docGUI:
         self.textbox = tk.Text(self.root, height=5, font=("Arial", 10))
         self.textbox.pack(padx=10, pady=10)
 
-        #make menu
+        #make menus
         menuBar = tk.Menu(self.root) #main menubar that contains all others
 
         fileMenu = tk.Menu(menuBar, tearoff=0)
@@ -56,8 +56,8 @@ class docGUI:
         fileMenu.add_command(label="Save", accelerator="Ctrl+S")
         fileMenu.add_command(label="Save As", accelerator="Ctrl+Shift+S")
         fileMenu.add_command(label="Rename")
-        fileMenu.add_command(label="Save All")
         fileMenu.add_separator()
+        fileMenu.add_command(label="Export")
         fileMenu.add_command(label="Print")
         fileMenu.add_command(label="Page Setup", accelerator="Ctrl+P")
 
@@ -69,6 +69,7 @@ class docGUI:
         editMenu.add_command(label="Cut", accelerator="Ctrl+X")
         editMenu.add_command(label="Copy", accelerator="Ctrl+C")
         editMenu.add_command(label="Paste", accelerator="Ctrl+Z")
+        editMenu.add_command(label="Select All", accelerator="Ctrl+A")
         editMenu.add_separator()
         editMenu.add_command(label="Find", accelerator="Ctrl+F")
         editMenu.add_command(label="Replace", accelerator="Ctrl+H")
@@ -77,14 +78,23 @@ class docGUI:
         menuBar.add_cascade(menu=formatMenu, label="Format")
         headingSubMenu = tk.Menu(formatMenu, tearoff=0)
         formatMenu.add_cascade(menu=headingSubMenu, label="Heading")
-        headingSubMenu.add_command(label="Normal")
-        headingSubMenu.add_command(label="Heading 1")
-        headingSubMenu.add_command(label="Heading 2")
-        headingSubMenu.add_command(label="Heading 3")
-        headingSubMenu.add_command(label="Heading 4")
-        headingSubMenu.add_command(label="Heading 5")
-        headingSubMenu.add_command(label="Heading 6")
-
+        headingSubMenu.add_radiobutton(label="Normal")
+        headingSubMenu.add_radiobutton(label="Heading 1")
+        headingSubMenu.add_radiobutton(label="Heading 2")
+        headingSubMenu.add_radiobutton(label="Heading 3")
+        headingSubMenu.add_radiobutton(label="Heading 4")
+        headingSubMenu.add_radiobutton(label="Heading 5")
+        headingSubMenu.add_radiobutton(label="Heading 6")
+        formatMenu.add_checkbutton(label="Italics", accelerator="Ctrl+I")
+        formatMenu.add_checkbutton(label="Bold", accelerator="Ctrl+B")
+        formatMenu.add_checkbutton(label="Underline", accelerator="Ctrl+U")
+        formatMenu.add_checkbutton(label="Strikethrough", accelerator="Ctrl+K")
+        formatMenu.add_checkbutton(label="Superscript", accelerator="Ctrl+^")
+        formatMenu.add_checkbutton(label="Subscript", accelerator="Ctrl+_")
+        formatMenu.add_separator()
+        formatMenu.add_command(label="Align Text Left", accelerator="Ctrl+{")
+        formatMenu.add_command(label="Align Text Center", accelerator="Ctrl+|")
+        formatMenu.add_command(label="Align Text Right", accelerator="Ctrl+}")
 
         toolsMenu = tk.Menu(menuBar, tearoff=0)
         menuBar.add_cascade(menu=toolsMenu, label="Tools")
